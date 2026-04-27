42f054f7a6d0f037362748a375ed68a25315c865
Orne <napoliorne2002@gmail.com>
2026-04-26 21:42:49 -0300

    arreglé error build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
+
 import './App.css'
 import Saludo from './componentes/saludo';
 import Titulo from './componentes/titulo';
@@ -13,7 +11,7 @@ import ListaTareas from './componentes/listatareas';
 import ToggleMensaje from './componentes/togglemensaje';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
